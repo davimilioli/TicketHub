c2e77fa78a080bcad200d684a078f9398e6defa5
Davi Milioli <davimilioli2108@gmail.com>
2024-07-30 20:22:54 -0300

    informações status de tickets

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { TicketService } from '../../services/ticket/ticket.service';
 import { Ticket, TicketList } from '../../Types';
 import { Router } from '@angular/router';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -12,14 +11,23 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 export class HomeComponent {
   ticketResult: TicketList = { total: 0, page: 1, pageSize: 12, tickets: [] };
   errorMessage: string = '';
+  ticketActives: number = 0;
+  ticketInactives: number = 0;
+  ticketPriority: { [key: string]: number } = {
+    'Baixa': 0,
+    'Normal': 0,
+    'Alta': 0,
+    'Urgente': 0
+  }
 
   constructor(private ticketService: TicketService, private router: Router){}
 
   ngOnInit(){
-
     this.ticketService.getAll().subscribe(
       result => {
         this.ticketResult = result
+        this.countStatus();
+        this.countPriorities()
         console.log(this.ticketResult);
       }, 
       error => {
@@ -31,5 +39,25 @@ export class HomeComponent {
 
   redirectTicket(ticket: Ticket): void{
     this.router.navigate(['/detalhe/', ticket.id])
+  }
+
+  countStatus(){
+    this.ticketActives = this.ticketResult.tickets.filter(ticket => ticket.status === 'Ativo').length;
+    this.ticketInactives = this.ticketResult.tickets.filter(ticket => ticket.status === 'Inativo').length;
+  }
+
+  countPriorities(){
+
+    for(const priority of Object.keys(this.ticketPriority)){
+      this.ticketPriority[priority] = 0;
+    };
+
+    this.ticketResult.tickets.forEach((ticket) => {
+      if(this.ticketPriority.hasOwnProperty(ticket.prioridade)){
+        this.ticketPriority[ticket.prioridade]++
+      }
+    })
+
+    console.log(this.ticketPriority);
   }
 }
